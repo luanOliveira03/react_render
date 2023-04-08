ec3f3b7638e5e57fb694214494092670888c24c9
luanOliveira03 <luan00534@gmail.com>
2023-04-08 15:00:50 -0300

    React Render 4x3 with Buttons

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, Button } from 'react-native';
 
-export default function App() {
+const Table = () => {
+  const renderRow = (rowIndex) => {
+    return (
+      <View key={rowIndex} style={styles.row}>
+        {renderCell(rowIndex, 1)}
+        {renderCell(rowIndex, 2)}
+        {renderCell(rowIndex, 3)}
+      </View>
+    );
+  };
+
+  const renderCell = (rowIndex, colIndex) => {
+    return (
+      <View key={colIndex} style={styles.cell}>
+        <Button title={`+`} /> 
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {renderRow(1)}
+      {renderRow(2)}
+      {renderRow(3)}
+      {renderRow(4)}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  cell: {
+    marginLeft: 10,
   },
 });
+
+export default Table;
